28fcccbeaf40459cabe4511d206bce569a130ca4
Leonardo Mira de Orduña <15582294.clot@fje.edu>
2026-05-15 12:44:54 +0200

    Main menu and quit

--- Assets/Scritps/MainMenu.cs
+++ Assets/Scritps/MainMenu.cs
@@ -3,7 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
- 
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Escena Principal");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 
 
 }
